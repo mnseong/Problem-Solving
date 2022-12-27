684f4d09c4d02289e37e610dad970dbd2d736e54
mnseong <mskweon@icloud.com>
2022-12-27 20:52:07 +0900

    [boj] #15650 - N과 M (4)

---
+++ boj/backtracking/15652.py
@@ -0,0 +1,22 @@
+"""
+"N과 M (4)"
+"https://www.acmicpc.net/problem/15652"
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+def dfs(arr, idx):
+    if len(arr) == m:
+        print(*arr)
+        return
+    for i in range(idx, n):
+        arr.append(numbers[i])
+        dfs(arr, i)
+        arr.pop()
+
+
+n, m = map(int, input().split())
+numbers = [i for i in range(1, n + 1)]
+dfs([], 0)
