d495271663608115aa9d6c363d56c44de1ee0412
mnseong <mskweon@icloud.com>
2022-12-27 20:42:03 +0900

    [boj] #15650 - 평범한 배낭

---
+++ boj/backtracking/15650.py
@@ -0,0 +1,23 @@
+'''
+"N과 M (2)"
+"https://www.acmicpc.net/problem/15650"
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(arr, idx):
+    if len(arr) == m:
+        print(*arr)
+        return
+    for i in range(idx, n):
+        arr.append(numbers[i])
+        dfs(arr, i + 1)
+        arr.pop()
+
+
+n, m = map(int, input().split())
+numbers = [i for i in range(1, n + 1)]
+dfs([], 0)
