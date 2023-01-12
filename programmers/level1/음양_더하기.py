c71be5dd2a5f737178dc61761570e3834d78c0ff
mnseong <mskweon@icloud.com>
2023-01-12 11:43:07 +0900

    [programmers] 월간 코드 챌린지 - 음양 더하기

---
+++ programmers/level1/음양_더하기.py
@@ -0,0 +1,10 @@
+'''
+월간 코드 챌린지 - 음양 더하기
+"https://school.programmers.co.kr/learn/courses/30/lessons/82612"
+'''
+
+def solution(absolutes, signs):
+    for i in range(len(signs)):
+        if not signs[i]:
+            absolutes[i] = -absolutes[i]
+    return sum(absolutes)
